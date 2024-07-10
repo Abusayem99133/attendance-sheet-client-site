2b1b9edb61980a19383755f09916391c56a0e1da
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-07-10 06:35:38 +0600

    firebase  resetup done

--- src/Components/firebase/firebase.config.js
+++ src/Components/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCmKJpxBzu09651nWIYEDi_Oego_hY9rj8",
-  authDomain: "attendance-sheet-5bf44.firebaseapp.com",
-  projectId: "attendance-sheet-5bf44",
-  storageBucket: "attendance-sheet-5bf44.appspot.com",
-  messagingSenderId: "716125416383",
-  appId: "1:716125416383:web:0d29c49ba826e7bf250f0a",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
